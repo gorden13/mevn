04b862f9ff50f6c7a97051499d24b383ca3e6c65
denis.goryncev <denis.goryncev@roscomputing.com>
2020-04-12 13:00:59 +0300

    server

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use('/api/link', require('./routes/link-routes'))
 app.use('/api/t', require('./routes/redirect-routes'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'cleint', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
 }
 
